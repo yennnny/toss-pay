40286b3c0366fbd28022afce449cd3ed58b51f2e
yeeun <84173008+jye104@users.noreply.github.com>
2025-01-28 01:11:33 +0900

    fix: fix setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/component/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    colors: {
+      primary: '#3283F1',
+      white: '#FFF',
+      black: '#10141E',
+      gray: {
+        100: '#F3F3F3',
+        200: '#F2F2F4',
+        700: '#707987',
+        800: '#616974',
+        900: '#1A1E27',
+      },
+      blue: {
+        100: '#E8F3FF',
       },
     },
   },
